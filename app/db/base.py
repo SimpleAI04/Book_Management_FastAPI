39b92bb03909c7fc0c6ec678b1c23924e7221ccb
Bisu0301 <dongquan312004@gmail.com>
2026-03-06 14:57:13 +0700

    create schema and api category

--- app/db/base.py
+++ app/db/base.py
@@ -2,9 +2,6 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-from app.models.author import Author
-from app.models.categoty import Category
-from app.models.book import Book
 
 
 
